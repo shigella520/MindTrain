1a9beb9de25753b3c21ed72b101db033875d6b8b
JianyuTan <shigella520@outlook.com>
2026-07-20 18:09:40 +0800

    test: isolate question revision integration state

--- apps/trainer-core/src/test/java/io/github/shigella520/mindtrain/core/QuestionRevisionIntegrationTest.java
+++ apps/trainer-core/src/test/java/io/github/shigella520/mindtrain/core/QuestionRevisionIntegrationTest.java
@@ -14,12 +14,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class QuestionRevisionIntegrationTest {
     @Autowired MockMvc mvc;
     @Autowired ObjectMapper objectMapper;
